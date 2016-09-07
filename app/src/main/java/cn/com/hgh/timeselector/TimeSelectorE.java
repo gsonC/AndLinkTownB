d884ace576041ff240041c76391f55b040ad6374
zemin.zheng <zemin.zheng@lincomb.com>
2016-09-07 17:31:47 +0800

    测试提交

--- app/src/main/java/cn/com/hgh/timeselector/TimeSelectorE.java
+++ app/src/main/java/cn/com/hgh/timeselector/TimeSelectorE.java
@@ -8,7 +8,6 @@ import android.content.Context;
 import android.text.TextUtils;
 import android.view.Gravity;
 import android.view.View;
-
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
