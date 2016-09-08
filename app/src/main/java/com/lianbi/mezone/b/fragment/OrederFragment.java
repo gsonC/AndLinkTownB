346551612c67f0f716d09fb21859e1c32139903f
chen.sun <chen.sun@lincomb.com>
2016-09-08 16:42:29 +0800

    订单管理

--- app/src/main/java/com/lianbi/mezone/b/fragment/OrederFragment.java
+++ app/src/main/java/com/lianbi/mezone/b/fragment/OrederFragment.java
@@ -1,334 +1,87 @@
 package com.lianbi.mezone.b.fragment;
-import android.graphics.Color;
+
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.ListAdapter;
-import android.widget.ListView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.lianbi.mezone.b.bean.InfoMessageBean;
-import com.lianbi.mezone.b.ui.InfoDetailsActivity;
+import com.lianbi.mezone.b.ui.OrderContentActivity;
 import com.xizhi.mezone.b.R;
 
 import java.util.ArrayList;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import cn.com.hgh.baseadapter.BaseAdapterHelper;
 import cn.com.hgh.baseadapter.QuickAdapter;
 import cn.com.hgh.view.SlideListView2;
 
 public class OrederFragment extends Fragment {
 
-	private SlideListView2 fm_messagefragment_listView;
-	private ImageView fm_messagefragment_iv_empty, iv_selectall;
-	ArrayList<InfoMessageBean> mDatas = new ArrayList<InfoMessageBean>();
-	private TextView tv_leavemessage;
-	private TextView time;
-	private TextView tv_tablename;
-	private LinearLayout tv_info;
-    private TextView tv_chshenhe;
-	private RelativeLayout ray_choice;
-	public static final int LEAVINGMESSAGE  = 101;//代表留言界面
+    @Bind(R.id.fm_orederfragment_listView)
+    SlideListView2 fmOrederfragmentListView;
+    @Bind(R.id.fm_orederfragment_iv_empty)
+    ImageView fmOrederfragmentIvEmpty;
+    private OrderContentActivity mActivity;
+    public QuickAdapter<InfoMessageBean> mAdapter;
+    ArrayList<InfoMessageBean> mDatas = new ArrayList<InfoMessageBean>();
 
-	/**
-	 * 是否删除
-	 */
-	boolean isDeted;
-	private InfoDetailsActivity mActivity;
-	// private OkHttpsImp okHttpsImp;
-//	private TextView tv_deletemessage, tv_toexamine, tv_seleteall;
-  
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fm_ordercontent, null);
+        mActivity = (OrderContentActivity) getActivity();
+        initView(view);
+        ButterKnife.bind(this, view);
+        return view;
+    }
 
-	// private ListView listView;
-	@Override
-	public View onCreateView(LayoutInflater inflater,
-			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.fm_leavemessage, null);
-		mActivity = (InfoDetailsActivity) getActivity();
-//		initView(view);
-//		listen();
-//		initListAdapter();
-		return view;
-	}
-	public ArrayList<InfoMessageBean> getLeaveMessage(){
+    private void initView(View view) {
 
-		return  mDatas;
-	}
-	private void initView(View view) {
-		fm_messagefragment_listView = (SlideListView2) view
-				.findViewById(R.id.fm_messagefragment_listView);
-		fm_messagefragment_listView.initSlideMode(SlideListView2.MOD_RIGHT);
+    }
+    /**
+     * 初始化list Adapter
+     */
+    private void initListAdapter() {
+        mAdapter = new QuickAdapter<InfoMessageBean>(mActivity,
+                R.layout.leavemessageitem, mDatas) {
 
-		fm_messagefragment_iv_empty = (ImageView) view
-				.findViewById(R.id.fm_messagefragment_iv_empty);
-//		tv_deletemessage = (TextView) view.findViewById(R.id.tv_deletemessage);// 删除
-//		tv_toexamine = (TextView) view.findViewById(R.id.tv_toexamine);// 审核
-//		iv_selectall = (ImageView) view.findViewById(R.id.iv_selectall);// 全选
-//		tv_seleteall = (TextView) view.findViewById(R.id.tv_seleteall);// 全选
-//		ray_choice= (RelativeLayout) view.findViewById(R.id.ray_choice);
+            @Override
+            protected void convert(final BaseAdapterHelper helper,
+                                   final InfoMessageBean item) {
+                TextView  tv_info = helper.getView(R.id.tv_info);
+                ImageView iv_check = helper.getView(R.id.iv_check);
+                TextView tv_chshenhe=helper.getView(R.id.tv_chshenhe);
+                TextView tv_tablename = helper.getView(R.id.tv_tablename);
+                TextView tv_leavemessage = helper.getView(R.id.tv_leavemessage);
+                TextView time = helper.getView(R.id.tv_time);
+                tv_leavemessage = helper.getView(R.id.tv_leavemessage);
 
-	}
-	public void afterToexamine(){
-//		for(int i=0;i<mDatas.size();i++){
-//			if(!"0".equals(mDatas.get(i).getAuditStatus())){
-//				tv_tablename.setTextColor(Color.BLACK);
-//				tv_leavemessage.setTextColor(Color.BLACK);
-//				time.setTextColor(Color.BLACK);
-//			}
-//		}
+                tv_tablename.setText(item.getTableName());
+                time.setText(String.valueOf(item.getCreateTime()));
+                tv_leavemessage.setText(item.getContent());
 
-	}
-	public  void   upDateFragment(boolean isSeleteAll ){
-		if(isSeleteAll){
-			for(int i=0;i<mDatas.size();i++){
-				mDatas.get(i).setS(true);
-			}
-			mAdapter.replaceAll(mDatas);
-		}else{
-			for(int i=0;i<mDatas.size();i++){
-				mDatas.get(i).setS(false);
-			}
-			mAdapter.replaceAll(mDatas);
-		}
-	}
-	private void listen() {
-//		ray_choice.setOnClickListener(new OnClickListener() {
-//
-//			@Override
-//			public void onClick(View v) {
-//
-//				if (isSeleteAll) {
-//					isSeleteAll = false;
-//					iv_selectall
-//							.setBackgroundResource(R.mipmap.message_unchecked);
-//					tv_seleteall.setText("全选");
-//					for (int i = 0; i < mDatas.size(); i++) {
-//						mDatas.get(i).setS(false);
-//					}
-//					mAdapter.replaceAll(mDatas);
-//				} else {
-//					isSeleteAll = true;
-//					iv_selectall
-//							.setBackgroundResource(R.mipmap.message_checked);
-//					tv_seleteall.setText("全不选");
-//					for (int i = 0; i < mDatas.size(); i++) {
-//						mDatas.get(i).setS(true);
-//					}
-//					mAdapter.replaceAll(mDatas);
-//				}
-//
-//			}
-//		});
-//		tv_deletemessage.setOnClickListener(new OnClickListener() {
-//
-//			@Override
-//			public void onClick(View v) {
-//				setExamineAndDelete(true);
-//
-//			}
-//
-//		});
-//
-//
-//
-//		tv_toexamine.setOnClickListener(new OnClickListener() {
-//			@Override
-//			public void onClick(View v) {
-//				for(int i=0;i<mDatas.size();i++){
-//					if(!"0".equals(mDatas.get(i).getAuditStatus())){
-//						tv_tablename.setTextColor(Color.RED);
-//						tv_leavemessage.setTextColor(Color.RED);
-//						time.setTextColor(Color.RED);
-//					}
-//				}
-//				setExamineAndDelete(false);
-//
-//			}
-//		});
-		}
-	
+                helper.getView(R.id.tv_chdelete).setOnClickListener(// 删除
+                        new View.OnClickListener() {
 
+                            @Override
+                            public void onClick(View v) {
 
-//	private void setExamineAndDelete(boolean status) {
-//		int s = mDatas.size();
-//		ArrayList<String> ids = new ArrayList<String>();
-//		for (int i = 0; i < s; i++) {
-//			if (mDatas.get(i).isS()) {
-//				ids.add(mDatas.get(i).getId() + "");
-//			}
-//		}
-//		if (status) {// 删除
-//			mActivity.delteMsg(ids, status);
-//			iv_selectall
-//			.setBackgroundResource(R.mipmap.message_unchecked);
-//		} else {// 审核
-//			mActivity.delteMsg(ids, status);
-//			iv_selectall
-//			.setBackgroundResource(R.mipmap.message_unchecked);
-//			tv_seleteall.setText("全选");
-//		}
-//	}
+                            }
+                        });
+            }
+        };
 
-
-	public static void setListViewHeightBasedOnChildren(ListView listView) {
-		ListAdapter listAdapter = listView.getAdapter();
-		if (listAdapter == null) {
-			return;
-		}
-		int totalHeight = 0;
-		for (int i = 0; i < listAdapter.getCount(); i++) {
-			View listItem = listAdapter.getView(i, null, listView);
-			listItem.measure(0, 0);
-			totalHeight += listItem.getMeasuredHeight();
-		}
-      
-		ViewGroup.LayoutParams params = listView.getLayoutParams();
-
-		params.height = totalHeight
-				+ (listView.getDividerHeight() * (listAdapter.getCount() - 1));
-		listView.setLayoutParams(params);
-	}
-
-	public QuickAdapter<InfoMessageBean> mAdapter;
-
-	int id;
-
-	/**
-	 * 初始化list Adapter
-	 */
-	private void initListAdapter() {
-		mAdapter = new QuickAdapter<InfoMessageBean>(mActivity,
-				R.layout.leavemessageitem, mDatas) {
-
-			@Override
-			protected void convert(final BaseAdapterHelper helper,
-					final InfoMessageBean item) {
-				id = item.getId();
-				tv_info = helper.getView(R.id.tv_info);
-				ImageView iv_check = helper.getView(R.id.iv_check);
-                tv_chshenhe=helper.getView(R.id.tv_chshenhe);
-				tv_tablename = helper.getView(R.id.tv_tablename);
-				tv_leavemessage = helper.getView(R.id.tv_leavemessage);
-				time = helper.getView(R.id.tv_time);
-               
-				tv_leavemessage = helper.getView(R.id.tv_leavemessage);
-				TextView time = helper.getView(R.id.tv_time);
-				LinearLayout tv_info = helper.getView(R.id.tv_info);
-
-
-				tv_tablename.setText(item.getTableName());
-				time.setText(String.valueOf(item.getCreateTime()));
-				tv_leavemessage.setText(item.getContent());
-				
-				
-				helper.getView(R.id.tv_chdelete).setOnClickListener(// 删除
-						new OnClickListener() {
-
-							@Override
-							public void onClick(View v) {
-								
-								fm_messagefragment_listView.slideBack();
-								// 通知服务器
-								mDatas.remove(item);
-								//Toast.makeText(mActivity, "删除", 0).show();
-								mAdapter.replaceAll(mDatas);
-								ArrayList<String> ids = new ArrayList<String>();
-								ids.add(String.valueOf(item.getId()));
-								mActivity.delteLeaveMsg(ids,true);
-							} 
-						});
-				if ("1".equals(item.getAuditStatus())) {
-					tv_chshenhe.setVisibility(View.GONE);
-	           
-				}
-				if ("0".equals(item.getAuditStatus())) {
-					tv_tablename.setTextColor(0xff18b08a);
-					tv_leavemessage.setTextColor(0xff18b08a);
-					time.setTextColor(0xff18b08a);
-
-				
-					helper.getView(R.id.tv_chshenhe).setOnClickListener(// 审核
-							new OnClickListener() {
-								
-								@Override
-								public void onClick(View v) {
-									fm_messagefragment_listView.slideBack();
-									// 通知服务器
-									mDatas.remove(item);
-									//Toast.makeText(mActivity, "审核", 0).show();
-									mAdapter.replaceAll(mDatas);
-									ArrayList<String> ids = new ArrayList<String>();
-									ids.add(String.valueOf(item.getId()));
-									mActivity.delteLeaveMsg(ids,false);
-									
-								}
-							});
-				}
-					else {
-						tv_tablename.setTextColor(Color.BLACK);
-						tv_leavemessage.setTextColor(Color.BLACK);
-						time.setTextColor(Color.BLACK);
-				}
-				if (item.isS()) {
-					helper.setImageResource(R.id.iv_check,
-							R.mipmap.message_checked);
-
-				} else {
-					helper.setImageResource(R.id.iv_check,
-							R.mipmap.message_unchecked);
-				}
-
-				
-				iv_check.setOnClickListener(new OnClickListener() {
-
-					@Override
-					public void onClick(View v) {
-						if (item.isS()) {
-							item.setS(false);
-							mAdapter.replaceAll(mDatas);
-						} else {
-							item.setS(true);
-							mAdapter.replaceAll(mDatas);
-						}
-//						mActivity.setCurrentList(mDatas);
-					}
-				});
-				
-
-			}
-		};
-
-		fm_messagefragment_listView.setAdapter(mAdapter);
-	}
-
-	/**
-	 * 是否删除
-	 */
-	public void doSomething(boolean isD, boolean isDel,
-			ArrayList<InfoMessageBean> cuArrayList) {
-		isDeted = isD;
-		
-		if (cuArrayList != null && cuArrayList.size() > 0) {
-			mDatas = cuArrayList;
-			mAdapter.replaceAll(mDatas);
-			fm_messagefragment_iv_empty.setVisibility(View.GONE);
-			fm_messagefragment_listView.setVisibility(View.VISIBLE);
-		} else {
-			if (mDatas.size() > 0) {
-				fm_messagefragment_iv_empty.setVisibility(View.GONE); 
-				fm_messagefragment_listView.setVisibility(View.VISIBLE);
-				mAdapter.replaceAll(mDatas);
-			} else {
-				fm_messagefragment_iv_empty.setVisibility(View.VISIBLE);
-				fm_messagefragment_listView.setVisibility(View.GONE);
-			}
-		}
-	}
+        fmOrederfragmentListView.setAdapter(mAdapter);
+    }
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
 }
--- app/src/main/java/com/lianbi/mezone/b/ui/OrderContentActivity.java
+++ app/src/main/java/com/lianbi/mezone/b/ui/OrderContentActivity.java
@@ -16,62 +16,141 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.lianbi.mezone.b.fragment.OrederFragment;
 import com.xizhi.mezone.b.R;
 
-import java.util.ArrayList;
-
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.com.hgh.timeselector.TimeSelectorE;
+import cn.com.hgh.utils.AbDateUtil;
+import cn.com.hgh.utils.AbStrUtil;
+import cn.com.hgh.utils.ContentUtils;
+import cn.com.hgh.view.NoScrollViewPager;
 
-public class OrderContentActivity extends BaseActivity {
+public class OrderContentActivity extends BaseActivity implements
+        ViewPager.OnPageChangeListener {
 
     public static final int POSITION0 = 0;
     public static final int POSITION1 = 1;
     public static final int POSITION2 = 2;
-    @Bind(R.id.valid_period_from)
-    TextView validPeriodFrom;
-    @Bind(R.id.valid_period_to)
-    TextView validPeriodTo;
-    @Bind(R.id.tv_allrecord)
-    TextView tvAllrecord;
-    @Bind(R.id.tv_accessrecord)
-    TextView tvAccessrecord;
-    @Bind(R.id.tv_userecord)
-    TextView tvUserecord;
-    @Bind(R.id.tv_integral_memberfile)
-    TextView tvIntegralMemberfile;
-    @Bind(R.id.tv_integral_recordsofconsumption)
-    TextView tvIntegralRecordsofconsumption;
-    @Bind(R.id.tv_integral_integralrecord)
-    TextView tvIntegralIntegralrecord;
-    @Bind(R.id.vp_orderpager)
-    ViewPager vpOrderpager;
-    @Bind(R.id.act_memberrecord_iv_empty)
-    ImageView actMemberrecordIvEmpty;
-    private int page = 1;
-    String position;
-    private ArrayList<Fragment> appFragments;
+
     OrederFragment mWholeFragment;
     OrederFragment mPaySuccessFragment;
     OrederFragment mPayFailFragment;
-    @OnClick({R.id.text_newcategory,R.id.tv_userecord})
+    @Bind(R.id.tv_starttime)
+    TextView tvStarttime;
+    @Bind(R.id.tv_finishtime)
+    TextView tvFinishtime;
+    @Bind(R.id.iv_close)
+    ImageView ivClose;
+    @Bind(R.id.tv_today)
+    TextView tvToday;
+    @Bind(R.id.tv_threeday)
+    TextView tvThreeday;
+    @Bind(R.id.tv_onemonth)
+    TextView tvOnemonth;
+    @Bind(R.id.tv_all)
+    TextView tvAll;
+    @Bind(R.id.tv_success)
+    TextView tvSuccess;
+    @Bind(R.id.tv_fail)
+    TextView tvFail;
+    @Bind(R.id.vp_orderpager)
+    NoScrollViewPager vpOrderpager;
+    @Bind(R.id.img_empty)
+    ImageView imgEmpty;
+    /**
+     * 当前位置
+     */
+    public int curPosition;
+    private final String ENDTIME = "2030-01-01 00:00";
+    private String coupName;
+    private String limitAmt;
+    private String coupAmt;
+    private String beginTime;
+    private String endTime;
+    @OnClick({R.id.tv_all, R.id.tv_success, R.id.tv_fail})
     public void OnClick(View v) {
         switch (v.getId()) {
 
-            case R.id.text_newcategory:
+            case R.id.tv_all:
                 vpOrderpager.setCurrentItem(0);
                 break;
-            case R.id.tv_integral_integralrecord:
+            case R.id.tv_success:
+                vpOrderpager.setCurrentItem(1);
+                break;
+            case R.id.tv_fail:
                 vpOrderpager.setCurrentItem(2);
                 break;
-            case R.id.tv_userecord:
-                vpOrderpager.setCurrentItem(3);
+        }
+    }
+    @OnClick({R.id.tv_today, R.id.tv_threeday, R.id.tv_onemonth})
+    public void OnDate(View v) {
+        switch (v.getId()) {
+
+            case R.id.tv_today:
+                vpOrderpager.setCurrentItem(0);
+                break;
+            case R.id.tv_threeday:
+                vpOrderpager.setCurrentItem(1);
+                break;
+            case R.id.tv_onemonth:
+                vpOrderpager.setCurrentItem(2);
+                break;
+        }
+    }
+    @OnClick({R.id.tv_starttime, R.id.tv_finishtime,R.id.iv_close})
+    public void OnTime(View v) {
+        switch (v.getId()) {
+
+            case R.id.tv_starttime:
+                TimeSelectorE timeSelectorFrom = new TimeSelectorE(OrderContentActivity.this,
+                        new TimeSelectorE.ResultHandler() {
+                            @Override
+                            public void handle(String time) {
+                                if (!AbStrUtil.isEmpty(endTime) && !AbDateUtil.compareTime(time, endTime)) {
+                                    ContentUtils.showMsg(OrderContentActivity.this, "结束日期须在开始日期之后！");
+                                    tvStarttime.setText("");
+                                    beginTime = "";
+                                } else {
+                                    tvStarttime.setText(time);
+                                    beginTime = time;
+                                }
+                            }
+                        }, AbDateUtil.getCurrentDate(AbDateUtil.dateFormatYMDHM),
+                        ENDTIME);
+                timeSelectorFrom.setMode(TimeSelectorE.MODE.YMD);
+                timeSelectorFrom.setTitle("起始时间");
+                timeSelectorFrom.show();
+                break;
+            case R.id.tv_finishtime:
+                TimeSelectorE timeSelectorTo = new TimeSelectorE(this,
+                        new TimeSelectorE.ResultHandler() {
+                            @Override
+                            public void handle(String time) {
+                                if (!AbStrUtil.isEmpty(beginTime) && !AbDateUtil.compareTime(beginTime, time)) {
+                                    ContentUtils.showMsg(OrderContentActivity.this, "结束日期须在开始日期之后！");
+                                    tvStarttime.setText("");
+                                    endTime = "";
+                                } else {
+                                    tvFinishtime.setText(time);
+                                    endTime = time;
+                                }
+                            }
+                        }, AbDateUtil.getCurrentDate(AbDateUtil.dateFormatYMDHM),
+                        ENDTIME);
+                timeSelectorTo.setMode(TimeSelectorE.MODE.YMD);
+                timeSelectorTo.setTitle("结束时间");
+                timeSelectorTo.show();
+                break;
+            case R.id.iv_close:
+
+                tvStarttime.setText("");
+                tvFinishtime.setText("");
                 break;
         }
     }
@@ -107,35 +186,19 @@ public class OrderContentActivity extends BaseActivity {
     }
 
     private void viewAdapter() {
-        if (mWholeFragment == null) {
-            mWholeFragment = new OrederFragment();
-        }
-        if (mPaySuccessFragment == null) {
-            mPaySuccessFragment = new OrederFragment();
-        }
-        if (mPayFailFragment == null) {
-            mPayFailFragment = new OrederFragment();
-        }
-        appFragments.add(mWholeFragment);
-        appFragments.add(mPaySuccessFragment);
-        appFragments.add(mPayFailFragment);
-        appFragments = new ArrayList<Fragment>();
-        vpOrderpager.setAdapter(new ViewPageAdapter(getSupportFragmentManager(), appFragments));
+        vpOrderpager.setAdapter(new MyAdapter(getSupportFragmentManager()));
         vpOrderpager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
 
             @Override
             public void onPageSelected(int arg0) {
                 switch (arg0) {
-                    case 0:
-                    {
+                    case 0: {
                     }
                     break;
-                    case 1:
-                    {
+                    case 1: {
                     }
                     break;
-                    case 2:
-                    {
+                    case 2: {
                     }
                     break;
                 }
@@ -167,38 +230,72 @@ public class OrderContentActivity extends BaseActivity {
         startActivity(new Intent(OrderContentActivity.this, MembersListActivity.class));
         finish();
     }
-    class ViewPageAdapter extends FragmentPagerAdapter {
-        private FragmentManager fManager;
-        private ArrayList<Fragment> mlist;
 
-        public ViewPageAdapter(FragmentManager fm, ArrayList<Fragment> list) {
+    class MyAdapter extends FragmentPagerAdapter {
+
+        public MyAdapter(FragmentManager fm) {
             super(fm);
-            fManager = fm;
-            mlist = list;
         }
 
         @Override
         public Fragment getItem(int position) {
-            return mlist.get(position);
+            switch (position) {
+                case POSITION0:
+                    if (mWholeFragment == null) {
+                        mWholeFragment = new OrederFragment();
+                    }
+                    return mWholeFragment;
+
+                case POSITION1:
+                    if (mPaySuccessFragment == null) {
+                        mPaySuccessFragment = new OrederFragment();
+                    }
+                    return mPaySuccessFragment;
+                case POSITION2:
+                    if (mPayFailFragment == null) {
+                        mPayFailFragment = new OrederFragment();
+                    }
+                    return mPayFailFragment;
+            }
+            return null;
         }
 
         @Override
         public int getCount() {
-            return mlist.size();
-        }
-
-        @Override
-        public Fragment instantiateItem(ViewGroup container, int position) {
-            Fragment fragment = (Fragment) super.instantiateItem(container, position);
-            fManager.beginTransaction().show(fragment).commit();
-            return fragment;
-        }
-
-        @Override
-        public void destroyItem(ViewGroup container, int position, Object object) {
-            Fragment fragment = mlist.get(position);
-            fManager.beginTransaction().hide(fragment).commit();
+            return 3;
         }
 
     }
+
+    @Override
+    public void onPageScrollStateChanged(int arg0) {
+
+    }
+
+    @Override
+    public void onPageScrolled(int arg0, float arg1, int arg2) {
+    }
+    @Override
+    public void onPageSelected(int arg0) {
+        curPosition = arg0;
+        switch (arg0) {
+            case POSITION0:
+                if (mWholeFragment != null) {
+
+                }
+                break;
+
+            case POSITION1:
+                if (mPaySuccessFragment != null) {
+
+                }
+                break;
+            case POSITION2:
+                if (mPayFailFragment != null) {
+
+                }
+                break;
+        }
+    }
+
 }
