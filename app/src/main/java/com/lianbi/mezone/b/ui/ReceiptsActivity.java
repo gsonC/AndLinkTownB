351d828bee50ec7a5e83500f59a5c18718396a55
zemin.zheng <zemin.zheng@lincomb.com>
2016-09-13 10:55:29 +0800

    提交更改

--- app/src/main/java/com/lianbi/mezone/b/ui/ReceiptsActivity.java
+++ app/src/main/java/com/lianbi/mezone/b/ui/ReceiptsActivity.java
@@ -1,15 +1,8 @@
 package com.lianbi.mezone.b.ui;
 
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.resource.drawable.GlideDrawable;
-import com.bumptech.glide.request.animation.GlideAnimation;
-import com.bumptech.glide.request.target.SimpleTarget;
 import com.xizhi.mezone.b.R;
 
 import butterknife.Bind;
@@ -19,8 +12,6 @@ import butterknife.ButterKnife;
  * 收款额度
  */
 public class ReceiptsActivity extends BaseActivity {
-    @Bind(R.id.background)
-    RelativeLayout background;
     @Bind(R.id.today_left_)
     TextView today_left;
     @Bind(R.id.today_total)
@@ -33,19 +24,5 @@ public class ReceiptsActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receipts, NOTYPE);
         ButterKnife.bind(ReceiptsActivity.this);
-        Glide.with(ReceiptsActivity.this).load(R.mipmap.payment).into(new SimpleTarget<GlideDrawable>() {
-            @Override
-            public void onResourceReady(GlideDrawable glideDrawable, GlideAnimation<? super GlideDrawable> glideAnimation) {
-                initBackground(glideDrawable);
-            }
-        });
-    }
-
-    private void initBackground(Drawable drawable) {
-        LinearLayout.LayoutParams lp = (LinearLayout.LayoutParams) background.getLayoutParams();
-        lp.weight = screenWidth;
-        lp.height = (int) ((double) (screenWidth * drawable.getMinimumHeight()) / (double) drawable.getMinimumWidth());
-        background.setLayoutParams(lp);
-        background.setBackgroundDrawable(drawable);
     }
 }
