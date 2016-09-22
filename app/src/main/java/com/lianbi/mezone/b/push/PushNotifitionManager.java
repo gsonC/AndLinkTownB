330060dc5facb377ea11d145c91994b1123ed8ab
xianwei.cheng <xianwei.cheng@LB14010011.lianbi.com.cn>
2016-09-22 18:45:04 +0800

    语音还原

--- app/src/main/java/com/lianbi/mezone/b/push/PushNotifitionManager.java
+++ app/src/main/java/com/lianbi/mezone/b/push/PushNotifitionManager.java
@@ -156,8 +156,8 @@ public class PushNotifitionManager {
 			//mTts.startSpeaking("买单了,老板娘打小票先", mSynListener);
 			mBuilder.setSound(Uri.parse("android.resource://" + mContext.getPackageName() + "/" + R.raw.checkorder));
 		} else if (4 == mDatas.getCallType()) {
-			//mBuilder.setSound(Uri.parse("android.resource://" + mContext.getPackageName() + "/" + R.raw.call));
-			mTts.startSpeaking(mDatas.getContent(), mSynListener);
+			mBuilder.setSound(Uri.parse("android.resource://" + mContext.getPackageName() + "/" + R.raw.call));
+			//mTts.startSpeaking(mDatas.getContent(), mSynListener);
 		} else if (6 == mDatas.getCallType()) {//收款成功
 			mTts.startSpeaking(mDatas.getContent(), mSynListener);
 		} else {
