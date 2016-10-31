015d80ea437ae00038d92fc8febebc27b6acb942
zemin.zheng <zemin.zheng@lincomb.com>
2016-10-31 14:24:58 +0800

    桌面详情空桌 添加桌面

--- app/src/main/java/com/lianbi/mezone/b/ui/AddTablesetActivity.java
+++ app/src/main/java/com/lianbi/mezone/b/ui/AddTablesetActivity.java
@@ -7,7 +7,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.lianbi.mezone.b.httpresponse.MyResultCallback;
-import com.lzy.okgo.OkGo;
 import com.xizhi.mezone.b.R;
 
 import org.json.JSONObject;
@@ -15,73 +14,74 @@ import org.json.JSONObject;
 import cn.com.hgh.utils.ContentUtils;
 import cn.com.hgh.utils.Result;
 
+/*
+* 添加桌面
+* */
 public class AddTablesetActivity extends BaseActivity {
 
-	private TextView tv_addtableset;
-	private EditText et_tablename;
+    private TextView tv_addtableset;
+    private EditText et_tablename;
+    private EditText et_table_persion_num;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.act_addtableset, NOTYPE);
-		initView();
-		setListener();
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_addtableset, NOTYPE);
+        initView();
+        setListener();
+    }
 
-	private void initView() {
-		setPageTitle("添加桌位");
-		tv_addtableset = (TextView) findViewById(R.id.tv_addtableset);
-		et_tablename = (EditText) findViewById(R.id.et_tablename);
-	}
+    private void initView() {
+        setPageTitle("添加桌位");
+        tv_addtableset = (TextView) findViewById(R.id.tv_addtableset);
+        et_tablename = (EditText) findViewById(R.id.et_tablename);
+        et_table_persion_num = (EditText) findViewById(R.id.et_table_persion_num);
+    }
 
-	private void setListener() {
-		tv_addtableset.setOnClickListener(this);
-	}
+    private void setListener() {
+        tv_addtableset.setOnClickListener(this);
+    }
 
-	@Override
-	protected void onChildClick(View view) {
-		super.onChildClick(view);
-		switch (view.getId()) {
-		case R.id.tv_addtableset:// 确定添加
-			String tablename = et_tablename.getText().toString().trim();
-			if (TextUtils.isEmpty(tablename)) {
-				ContentUtils.showMsg(AddTablesetActivity.this, "桌位名称不能为空");
-				return;
-			}
-			getAddTable(tablename);
-			break;
+    @Override
+    protected void onChildClick(View view) {
+        super.onChildClick(view);
+        switch (view.getId()) {
+            case R.id.tv_addtableset:// 确定添加
+                String tablename = et_tablename.getText().toString().trim();
+                if (TextUtils.isEmpty(tablename)) {
+                    ContentUtils.showMsg(AddTablesetActivity.this, "桌位名称不能为空");
+                    return;
+                }
+                getAddTable(tablename);
+                break;
 
-		}
-	}
+        }
+    }
 
-	private void getAddTable(String tablename) {
-		okHttpsImp.getAddTable(new MyResultCallback<String>() {
+    private void getAddTable(String tablename) {
+        okHttpsImp.getAddTable(new MyResultCallback<String>() {
 
-			@Override
-			public void onResponseResult(Result result) {
-				String reString = result.getData();
-				if (reString != null) {
-					JSONObject jsonObject;
-					try {
-						setResult(RESULT_OK);
-						finish();
-						ContentUtils.showMsg(AddTablesetActivity.this, "添加成功");
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-				}
-			}
+            @Override
+            public void onResponseResult(Result result) {
+                String reString = result.getData();
+                if (reString != null) {
+                    JSONObject jsonObject;
+                    try {
+                        setResult(RESULT_OK);
+                        finish();
+                        ContentUtils.showMsg(AddTablesetActivity.this, "添加成功");
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
 
-			@Override
-			public void onResponseFailed(String msg) {
+            @Override
+            public void onResponseFailed(String msg) {
 
-			}
-		}, userShopInfoBean.getBusinessId(),tablename);
-		
-		
-		
-		
-		
-		
-	}
+            }
+        }, userShopInfoBean.getBusinessId(), tablename);
+
+
+    }
 }
--- app/src/main/java/com/lianbi/mezone/b/ui/ScanningQRActivity.java
+++ app/src/main/java/com/lianbi/mezone/b/ui/ScanningQRActivity.java
@@ -1,13 +1,10 @@
 package com.lianbi.mezone.b.ui;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
 
 import org.json.JSONObject;
 
@@ -25,12 +22,10 @@ import android.os.Handler;
 import android.os.Message;
 import android.provider.MediaStore;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import cn.com.hgh.utils.ContentUtils;
 import cn.com.hgh.utils.Result;
@@ -38,224 +33,226 @@ import cn.com.hgh.view.DialogCommon;
 import cn.com.hgh.view.HttpDialog;
 
 import com.alibaba.fastjson.JSON;
-import com.bumptech.glide.Glide;
 import com.xizhi.mezone.b.R;
-import com.lianbi.mezone.b.bean.Ades_ImageEs;
-import com.lianbi.mezone.b.bean.OrderListBean;
 import com.lianbi.mezone.b.bean.QrcodeDetails;
 import com.lianbi.mezone.b.httpresponse.MyResultCallback;
-import com.lianbi.mezone.b.httpresponse.OkHttpsImp;
 
+/*
+* 桌面详情--空桌
+* */
 public class ScanningQRActivity extends BaseActivity {
 
-	private TextView tv_tablename, tv_keepqr;
-	private ImageView iv_scanningqr;
-	HttpDialog dialog;
-	String tableid;
-	private String url,http;
-	private String imageUrl="";
-	QrcodeDetails  mQrcodeDetails;
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.act_scanningqr, NOTYPE);
-		initView();
-		listen();
-		dialog = new HttpDialog(this);
-		dialog.show();
-		getTableDetail(tableid);
-	}
-	public void getHttpBitmap(final String url,final ImageView  img){
-    	try{
-    		new Thread(){  
-    			  @Override  
-    			   public void run()  
-    			   {  
-    		    		try {
-    		    			URL	myFileURL = new URL(url);
-							//获得连接
-	    		    		HttpURLConnection conn=(HttpURLConnection)myFileURL.openConnection();
-	    		    		conn.setConnectTimeout(6000);
-	    		    		conn.setDoInput(true);
-	    		    		//不使用缓
-	    		    		conn.setUseCaches(false);
-	    		    		InputStream is = conn.getInputStream();
-	    		    		//解析得到图片
-	    		    		Bitmap bitmap = BitmapFactory.decodeStream(is);
-	    		    		//关闭数据流
-//	    		    		img.setImageBitmap(bitmap);
-	    					Message msg = new Message();
-							msg.obj=bitmap;
-							mHandler.sendMessage(msg);
-	    		    		is.close();  
-	    				  //把网络访问的代码放在这里    
-    		    		} catch (Exception e) {
-							e.printStackTrace();
-						}
-    		    		 
-    			  }  
-    			}.start();  
+    private TextView tv_tablename, tv_keepqr;
+    private ImageView iv_scanningqr;
+    HttpDialog dialog;
+    String tableid;
+    private String url, http;
+    private String imageUrl = "";
+    QrcodeDetails mQrcodeDetails;
 
-    	}catch(Exception e){
-    		e.printStackTrace();
-    	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_scanningqr, NOTYPE);
+        initView();
+        listen();
+        dialog = new HttpDialog(this);
+        dialog.show();
+        getTableDetail(tableid);
     }
-	@SuppressLint("HandlerLeak")
-	Handler mHandler = new Handler() {
-		
-    	public void handleMessage(Message msg) {
-    		iv_scanningqr.setImageBitmap((Bitmap) msg.obj);
 
-    		
-    	}
-		
-	};
+    public void getHttpBitmap(final String url, final ImageView img) {
+        try {
+            new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        URL myFileURL = new URL(url);
+                        //获得连接
+                        HttpURLConnection conn = (HttpURLConnection) myFileURL.openConnection();
+                        conn.setConnectTimeout(6000);
+                        conn.setDoInput(true);
+                        //不使用缓
+                        conn.setUseCaches(false);
+                        InputStream is = conn.getInputStream();
+                        //解析得到图片
+                        Bitmap bitmap = BitmapFactory.decodeStream(is);
+                        //关闭数据流
+//	    		    		img.setImageBitmap(bitmap);
+                        Message msg = new Message();
+                        msg.obj = bitmap;
+                        mHandler.sendMessage(msg);
+                        is.close();
+                        //把网络访问的代码放在这里
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
 
-	 
-	private void listen() {
-		tv_keepqr.setOnClickListener(this);
-		tv_tablename.setOnClickListener(new OnClickListener() {
+                }
+            }.start();
 
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent();
-				/* 开启Pictures画面Type设定为image */
-				intent.setType("image/*");
-				/* 使用Intent.ACTION_GET_CONTENT这个Action */
-				intent.setAction(Intent.ACTION_GET_CONTENT);
-				/* 取得相片后返回本画面 */
-				startActivity(intent);
-			}
-		});
-	}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	private void initView() {
-		setPageTitle("扫二维码");
-		String tablename = getIntent().getStringExtra("TABLENAME");
-		String tableqr = getIntent().getStringExtra("TABLEQR");
-		tableid=getIntent().getStringExtra("TABLEID");
+    @SuppressLint("HandlerLeak")
+    Handler mHandler = new Handler() {
+
+        public void handleMessage(Message msg) {
+            iv_scanningqr.setImageBitmap((Bitmap) msg.obj);
+
+
+        }
+
+    };
+
+
+    private void listen() {
+        tv_keepqr.setOnClickListener(this);
+        tv_tablename.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                /* 开启Pictures画面Type设定为image */
+                intent.setType("image/*");
+                /* 使用Intent.ACTION_GET_CONTENT这个Action */
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                /* 取得相片后返回本画面 */
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initView() {
+        setPageTitle("扫二维码");
+        String tablename = getIntent().getStringExtra("TABLENAME");
+        String tableqr = getIntent().getStringExtra("TABLEQR");
+        tableid = getIntent().getStringExtra("TABLEID");
 //		Glide.with(ScanningQRActivity.this).load(tableqr).error(R.id.defaultimg_11).into(iv_scanningqr);
-		tv_tablename = (TextView) findViewById(R.id.tv_tablename);
-		if (!(null == tablename && TextUtils.isEmpty(tablename))) {
-			tv_tablename.setText(tablename);
-		} else {
-			tv_tablename.setText("获取桌位名称失败");
-		}
-		tv_keepqr = (TextView) findViewById(R.id.tv_keepqr);
-		iv_scanningqr = (ImageView) findViewById(R.id.iv_scanningqr);
+        tv_tablename = (TextView) findViewById(R.id.tv_tablename);
+        if (!(null == tablename && TextUtils.isEmpty(tablename))) {
+            tv_tablename.setText(tablename);
+        } else {
+            tv_tablename.setText("获取桌位名称失败");
+        }
+        tv_keepqr = (TextView) findViewById(R.id.tv_keepqr);
+        iv_scanningqr = (ImageView) findViewById(R.id.iv_scanningqr);
 //		iv_scanningqr.setBackgroundResource(R.drawable.addtableset);
-	}
+    }
 
-	@Override
-	protected void onChildClick(View view) {
-		switch (view.getId()) {
-		case R.id.tv_keepqr:
-			try {
-				Bitmap bitmap = ((BitmapDrawable) ((ImageView) iv_scanningqr)
-						.getDrawable()).getBitmap();
-				if (null != bitmap) {
-					if (saveImageToGallery(this, bitmap)) {
-						DialogCommon dialogCommon = new DialogCommon(
-								ScanningQRActivity.this) {
+    @Override
+    protected void onChildClick(View view) {
+        switch (view.getId()) {
+            case R.id.tv_keepqr:
+                try {
+                    Bitmap bitmap = ((BitmapDrawable) ((ImageView) iv_scanningqr)
+                            .getDrawable()).getBitmap();
+                    if (null != bitmap) {
+                        if (saveImageToGallery(this, bitmap)) {
+                            DialogCommon dialogCommon = new DialogCommon(
+                                    ScanningQRActivity.this) {
 
-							@Override
-							public void onCheckClick() {
-								dismiss();
-							}
+                                @Override
+                                public void onCheckClick() {
+                                    dismiss();
+                                }
 
-							@Override
-							public void onOkClick() {
-								Intent intent = new Intent();
-								intent.setType("image/*");
-								intent.setAction(Intent.ACTION_GET_CONTENT);
-								startActivity(intent);
-								dismiss();
-							}
+                                @Override
+                                public void onOkClick() {
+                                    Intent intent = new Intent();
+                                    intent.setType("image/*");
+                                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                                    startActivity(intent);
+                                    dismiss();
+                                }
 
-						};
-						dialogCommon.setTextTitle("保存图片成功");
-						dialogCommon.setTv_dialog_common_ok("立即查看");
-						dialogCommon.setTv_dialog_common_cancel("稍后再看");
-						dialogCommon.show();
-					}else{
-						ContentUtils.showMsg(this, "保存图片失败");
-					}
-				}
-				
-			} catch (Exception e) {
+                            };
+                            dialogCommon.setTextTitle("保存图片成功");
+                            dialogCommon.setTv_dialog_common_ok("立即查看");
+                            dialogCommon.setTv_dialog_common_cancel("稍后再看");
+                            dialogCommon.show();
+                        } else {
+                            ContentUtils.showMsg(this, "保存图片失败");
+                        }
+                    }
 
-			e.printStackTrace();
-			
-			}
-			break;
+                } catch (Exception e) {
 
-		}
-	}
-	   public void getTableDetail(String tableId){
-	   	okHttpsImp.getTableDetail(new MyResultCallback<String>() {
+                    e.printStackTrace();
 
-			@Override
-			public void onResponseResult(Result result) {
-				String reString = result.getData();
-				if (reString != null) {
-					JSONObject jsonObject;
-					try {
-						jsonObject= new JSONObject(reString);
-						reString = jsonObject.getString("detail");
-						mQrcodeDetails=JSON.parseObject(
-								reString, QrcodeDetails.class);
-						url=mQrcodeDetails.getCodeUrl();
-						http=jsonObject.getString("httpInfo").toString();
-					    imageUrl=http+url;
+                }
+                break;
+
+        }
+    }
+
+    public void getTableDetail(String tableId) {
+        okHttpsImp.getTableDetail(new MyResultCallback<String>() {
+
+            @Override
+            public void onResponseResult(Result result) {
+                String reString = result.getData();
+                if (reString != null) {
+                    JSONObject jsonObject;
+                    try {
+                        jsonObject = new JSONObject(reString);
+                        reString = jsonObject.getString("detail");
+                        mQrcodeDetails = JSON.parseObject(
+                                reString, QrcodeDetails.class);
+                        url = mQrcodeDetails.getCodeUrl();
+                        http = jsonObject.getString("httpInfo").toString();
+                        imageUrl = http + url;
 //						Glide.with(ScanningQRActivity.this).load(imageUrl).error(R.drawable.about_us).into(iv_scanningqr);
-						getHttpBitmap(imageUrl, iv_scanningqr);
-					    
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-				}
-				dialog.dismiss();
-			}
+                        getHttpBitmap(imageUrl, iv_scanningqr);
 
-			@Override
-			public void onResponseFailed(String msg) {
-				ContentUtils.showMsg(ScanningQRActivity.this,msg);
-				dialog.dismiss();
-			}
-		}, userShopInfoBean.getBusinessId(),tableId);
-	   
-	   }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                dialog.dismiss();
+            }
 
-	public static boolean saveImageToGallery(Context context, Bitmap bmp) {
-		// 首先保存图片
-		File appDir = new File(Environment.getExternalStorageDirectory(),
-				"Boohee");
-		if (!appDir.exists()) {
-			appDir.mkdir();
-		}
-		String fileName = System.currentTimeMillis() + ".jpg";
-		File file = new File(appDir, fileName);
-		try {
-			FileOutputStream fos = new FileOutputStream(file);
-			bmp.compress(CompressFormat.JPEG, 100, fos);
-			fos.flush();
-			fos.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-			return false;
-		}
+            @Override
+            public void onResponseFailed(String msg) {
+                ContentUtils.showMsg(ScanningQRActivity.this, msg);
+                dialog.dismiss();
+            }
+        }, userShopInfoBean.getBusinessId(), tableId);
 
-		// 其次把文件插入到系统图库
-		try {
-			MediaStore.Images.Media.insertImage(context.getContentResolver(),
-					file.getAbsolutePath(), fileName, null);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return false;
-		}
-		// 最后通知图库更新
-		context.sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE,
-				Uri.fromFile(new File(file.getPath()))));
-		return true;
-	}
+    }
+
+    public static boolean saveImageToGallery(Context context, Bitmap bmp) {
+        // 首先保存图片
+        File appDir = new File(Environment.getExternalStorageDirectory(),
+                "Boohee");
+        if (!appDir.exists()) {
+            appDir.mkdir();
+        }
+        String fileName = System.currentTimeMillis() + ".jpg";
+        File file = new File(appDir, fileName);
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            bmp.compress(CompressFormat.JPEG, 100, fos);
+            fos.flush();
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        // 其次把文件插入到系统图库
+        try {
+            MediaStore.Images.Media.insertImage(context.getContentResolver(),
+                    file.getAbsolutePath(), fileName, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        // 最后通知图库更新
+        context.sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE,
+                Uri.fromFile(new File(file.getPath()))));
+        return true;
+    }
 }
